525af3fb2f1be9a62393a4e44a7c0ad2b07702b8
Istiak Hossain <istiakhossain.ab@gmail.com>
2025-09-02 12:50:35 +0600

    first commit

---
+++ Simple Calculator.js
@@ -0,0 +1,54 @@
+let currentInput = "";
+let operator = "";
+let firstValue = "";
+
+function appendNumber(num){
+      currentInput = currentInput + num; // String add
+      document.getElementById("display").value = currentInput;
+}
+
+function setOperator(op){
+    if(currentInput == "") return;
+    firstValue = currentInput;
+    operator = op;
+    currentInput = "";
+}
+
+function calculate(){
+    if(firstValue == "" || currentInput == "" || operator == ""){
+        return;
+    }
+
+    let result ;
+    let num1 = parseFloat(firstValue);
+    let num2 = parseFloat(currentInput);
+
+    switch(operator){
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            return;
+    }
+    document.getElementById("display").value = result;
+    currentInput = result.toString();
+    firstValue = "";
+    operator = "";
+}
+
+
+function cleareDisplay(){
+     currentInput = "";
+     operator = "";
+     firstValue = "";
+     document.getElementById("display").value = "0";
+}
